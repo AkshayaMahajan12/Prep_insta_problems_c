3135676bcc3625b00f6148134a567afa1bcc3599
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:31:37 +0530

    Update 02_even_odd.c

--- Getting_Started/02_even_odd.c
+++ Getting_Started/02_even_odd.c
@@ -1,22 +1,20 @@
-// Method 1
-// For user input number
+// Method 1: For user input number
 
-// #include<stdio.h>
-// void main(){
-//     int num;
+#include<stdio.h>
+void main(){
+    int num;
 
-//     printf("Enter a Number to check Even or Odd: ");
-//     scanf("%d",&num);
+    printf("Enter a Number to check Even or Odd: ");
+    scanf("%d",&num);
 
-//     if(num%2==0)
-//         printf("%d is a Even Number.",num);
+    if(num%2==0)
+        printf("%d is a Even Number.",num);
 
-//     else
-//         printf("%d is a Odd Number.",num);
-// }
+    else
+        printf("%d is a Odd Number.",num);
+}
 
-// Method 2
-// For this method, we use the ternary operator in C
+// Method 2: For this method, we use the ternary operator in C
 #include<stdio.h>
 
 void main(){
@@ -28,3 +26,4 @@ void main(){
     (num%2==0)?printf("%d is Even Number.",num):printf("%d is Odd Number.",num);
 
 }
+
