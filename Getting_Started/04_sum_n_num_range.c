14c3747d4566ee3f9b68096007c16f15b434fb8d
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:33:50 +0530

    Update 04_sum_n_num_range.c

--- Getting_Started/04_sum_n_num_range.c
+++ Getting_Started/04_sum_n_num_range.c
@@ -1,21 +1,21 @@
 // Method 1 : Using Brute Force
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int n1,n2,i;
-//     int sum;
-//     printf("Enter range to calculate sum(starting): ");
-//     scanf("%d",&n1);
-//     printf("Enter range to calculate sum(end): ");
-//     scanf("%d",&n2);
+void main(){
+    int n1,n2,i;
+    int sum;
+    printf("Enter range to calculate sum(starting): ");
+    scanf("%d",&n1);
+    printf("Enter range to calculate sum(end): ");
+    scanf("%d",&n2);
 
-//     for(i=n1;i<=n2;i++){
-//         sum+=i;
-//     }
-//     printf("Sum of n natural numbers in given Range is %d",sum);
+    for(i=n1;i<=n2;i++){
+        sum+=i;
+    }
+    printf("Sum of n natural numbers in given Range is %d",sum);
 
-// }
+}
 
 
 // Method 2 : Using the Formula
@@ -35,6 +35,7 @@ void main(){
 
     printf("Sum of n natural numbers in given Range is %d",sum);
 }
+
 
 
 
