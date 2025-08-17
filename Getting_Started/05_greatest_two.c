2f7cc11cdc2194c4f2b849cc4bfe3cdf83b02d11
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:35:35 +0530

    Update 05_greatest_two.c

--- Getting_Started/05_greatest_two.c
+++ Getting_Started/05_greatest_two.c
@@ -2,20 +2,20 @@
 
 // Method 1 : Using if – else Statements
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int num1,num2;
-//     printf("Enter number to compare: ");
-//     scanf("%d%d",&num1,&num2);
+void main(){
+    int num1,num2;
+    printf("Enter number to compare: ");
+    scanf("%d%d",&num1,&num2);
 
-//     if(num1==num2)
-//         printf("%d and %d are Equal.",num1,num2);
-//     else if(num1>num2)
-//         printf("%d is greater than %d.",num1,num2);
-//     else
-//         printf("%d is less than %d.",num1,num2);
-// }
+    if(num1==num2)
+        printf("%d and %d are Equal.",num1,num2);
+    else if(num1>num2)
+        printf("%d is greater than %d.",num1,num2);
+    else
+        printf("%d is less than %d.",num1,num2);
+}
 
 // Method 2 : Using Ternary Operator
 
@@ -34,4 +34,5 @@ void main(){
         (num1>num2)?printf("%d is greater than %d.",num1,num2):printf("%d is less than %d.",num1,num2);
 
 }
+
 
