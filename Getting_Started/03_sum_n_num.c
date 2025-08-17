512b705cbaff6687ffd09bcf5c518d49bc587ef4
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:32:52 +0530

    Update 03_sum_n_num.c

--- Getting_Started/03_sum_n_num.c
+++ Getting_Started/03_sum_n_num.c
@@ -1,49 +1,50 @@
 // Method 1: Iterative way (Using for loop)
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int n;
-//     int sum;
-//     printf("Enter a number: ");
-//     scanf("%d",&n);
+void main(){
+    int n;
+    int sum;
+    printf("Enter a number: ");
+    scanf("%d",&n);
 
-//     for(int i=0;i<n+1;i++){
-//         sum+=i;
-//     }
-//     printf("Sum of N Natural Numbers is %d",sum);
-// }
+    for(int i=0;i<n+1;i++){
+        sum+=i;
+    }
+    printf("Sum of N Natural Numbers is %d",sum);
+}
 
 
 // Method 2: Iterative way (Using while loop)
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int n,i=1;
-//     int sum;
-//     printf("Enter a number: ");
-//     scanf("%d",&n);
+void main(){
+    int n,i=1;
+    int sum;
+    printf("Enter a number: ");
+    scanf("%d",&n);
 
-//     while(i<=n){
-//         sum+=i;
-//         i++;
-//     }
-//     printf("Sum of N Natural Numbers is %d",sum);
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    printf("Sum of N Natural Numbers is %d",sum);
 
-// }
+}
 
 
 // Method 3: Direct Formula
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int n;
-//     int sum;
-//     printf("Enter a number: ");
-//     scanf("%d",&n);
-//     sum=(n*(n+1))/2;
-//     printf("Sum of n Natural Numbers is %d",sum);
-// }
+void main(){
+    int n;
+    int sum;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    sum=(n*(n+1))/2;
+    printf("Sum of n Natural Numbers is %d",sum);
+}
+
 
