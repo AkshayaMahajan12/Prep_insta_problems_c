3f25e0cb408f18feb7fcaff01bbfa7b525900169
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:29:49 +0530

    Update 01_check_pos_neg.c

--- Getting_Started/01_check_pos_neg.c
+++ Getting_Started/01_check_pos_neg.c
@@ -7,43 +7,43 @@
 
 // Method 1: Using Brute Force
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int num;
-//     printf("Enter a number to check Positive or Negative: ");
-//     scanf("%d",&num);
+void main(){
+    int num;
+    printf("Enter a number to check Positive or Negative: ");
+    scanf("%d",&num);
 
-//     if(num<0)
-//         printf("%d Number is Negative.",num);
+    if(num<0)
+        printf("%d Number is Negative.",num);
         
-//     else if(num>0)
-//         printf("%d Number is Positive.",num);
-//     else
-//         printf("Entered Number is Zero.");
-// }
+    else if(num>0)
+        printf("%d Number is Positive.",num);
+    else
+        printf("Entered Number is Zero.");
+}
 
 // Method 2: Using Nested if-else Statements
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int num;
+void main(){
+    int num;
     
-//     printf("Enter a number to check Positive or Negative: ");
-//     scanf("%d",&num);
+    printf("Enter a number to check Positive or Negative: ");
+    scanf("%d",&num);
 
-//     if(num>=0)
-//     {
-//         if(num>0)
-//             printf("%d is a Positive Number.",num);
-//         else
-//             printf("Entered Number is Zero(0).");
-//     }
-//     else
-//         printf("%d is a Negative Number.",num);
+    if(num>=0)
+    {
+        if(num>0)
+            printf("%d is a Positive Number.",num);
+        else
+            printf("Entered Number is Zero(0).");
+    }
+    else
+        printf("%d is a Negative Number.",num);
 
-// }
+}
 
 
 // Method 3: Using the ternary operator
@@ -62,5 +62,6 @@ void main(){
         printf("Entered Number is Zero(0).");
     else
         (num>0) ? printf("%d is a Positive Number.",num):printf("%d is a Negative Number.",num);
+
 
 }
