70462cfa359e8221f7fb5c6aca3311d694543c59
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:37:47 +0530

    Update 07_check_leap_year.c

--- Getting_Started/07_check_leap_year.c
+++ Getting_Started/07_check_leap_year.c
@@ -3,24 +3,24 @@
 // Method 1: Using if-else Statements 1
 
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int year;
+void main(){
+    int year;
 
-//     printf("Enter a year to check Leap or not: ");
-//     scanf("%d",&year);
+    printf("Enter a year to check Leap or not: ");
+    scanf("%d",&year);
 
-//     if(year%400==0)
-//         printf("Year %d is a Leap Year.",year);
-//     else if(year%100==0)
-//         printf("Year %d is a not Leap Year.",year);
-//     else if(year%4==0)
-//         printf("Year %d is a Leap Year.",year);
-//     else
-//         printf("Year %d is not a Leap Year.",year);
+    if(year%400==0)
+        printf("Year %d is a Leap Year.",year);
+    else if(year%100==0)
+        printf("Year %d is a not Leap Year.",year);
+    else if(year%4==0)
+        printf("Year %d is a Leap Year.",year);
+    else
+        printf("Year %d is not a Leap Year.",year);
 
-// }
+}
 
 // Method 2: Using if-else Statements 2
 
@@ -36,5 +36,6 @@ void main(){
         printf("year %d is a Leap Year.",year);
     else
         printf("Year %d is not a Leap Year.",year);
+
 
 }
