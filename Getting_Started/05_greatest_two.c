0d7faf8d902a73be7329acef316663f2d1f6e2e2
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:34:58 +0530

    Add files via upload

---
+++ Getting_Started/05_greatest_two.c
@@ -0,0 +1,37 @@
+// Find the Largest of the Two Numbers in C
+
+// Method 1 : Using if – else Statements
+
+// #include<stdio.h>
+
+// void main(){
+//     int num1,num2;
+//     printf("Enter number to compare: ");
+//     scanf("%d%d",&num1,&num2);
+
+//     if(num1==num2)
+//         printf("%d and %d are Equal.",num1,num2);
+//     else if(num1>num2)
+//         printf("%d is greater than %d.",num1,num2);
+//     else
+//         printf("%d is less than %d.",num1,num2);
+// }
+
+// Method 2 : Using Ternary Operator
+
+
+#include<stdio.h>
+
+void main(){
+    int num1,num2;
+
+    printf("Enter number to compare: ");
+    scanf("%d%d",&num1,&num2);
+
+    if(num1==num2)
+        printf("%d and %d are Equal.",num1,num2);
+    else
+        (num1>num2)?printf("%d is greater than %d.",num1,num2):printf("%d is less than %d.",num1,num2);
+
+}
+
