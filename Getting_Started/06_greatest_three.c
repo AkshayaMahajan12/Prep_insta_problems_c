33a532ccc39d81a05155fa848936d69511904f41
AkshayaMahajan12 <akshayamahajan3@gmail.com>
2025-08-17 15:36:40 +0530

    Update 06_greatest_three.c

--- Getting_Started/06_greatest_three.c
+++ Getting_Started/06_greatest_three.c
@@ -2,27 +2,27 @@
 
 // Method 1: Using if-else Statements 1
 
-// #include<stdio.h>
+#include<stdio.h>
 
-// void main(){
-//     int num1,num2,num3;
+void main(){
+    int num1,num2,num3;
 
-//     printf("Enter three numbers  to check equality: ");
-//     scanf("%d%d%d",&num1,&num2,&num3);
+    printf("Enter three numbers  to check equality: ");
+    scanf("%d%d%d",&num1,&num2,&num3);
 
-//     if(num1==num2 && num1==num3)
-//         printf("All values are Equal.");
+    if(num1==num2 && num1==num3)
+        printf("All values are Equal.");
 
-//     else if(num1>=num2 && num1>=num3)
-//         printf("%d is greatest.",num1);
+    else if(num1>=num2 && num1>=num3)
+        printf("%d is greatest.",num1);
 
-//     else if(num2>=num1 && num2>=num3)
-//         printf("%d is greatest.",num2);
+    else if(num2>=num1 && num2>=num3)
+        printf("%d is greatest.",num2);
 
-//     else if(num3>=num1 && num3>=num2)
-//         printf("%d is greatest.",num3);
+    else if(num3>=num1 && num3>=num2)
+        printf("%d is greatest.",num3);
 
-// }
+}
 
 // Method 2: Using if-else Statements 2
 
@@ -68,5 +68,6 @@ void main(){
         result=(temp>num3)?temp:num3;
 
         printf("%d is the greatest.",result);
+
 
 }
